5a9eb991f1adc4f94dc6e72015053d4ca10332a3
pathakakshay <pathakakshay1111@gmail.com>
2020-12-16 17:52:32 +0530

    first init

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Your Website Title</title>
+<!-- You can use Open Graph tags to customize link previews.
+Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
+<meta property="og:url"           content="http://testingakshay/" />
+<meta property="og:type"          content="website" />
+<meta property="og:title"         content="Best free blog sites to consider in 2020" />
+<meta property="og:description"   content="Here are the best free blogging sites you can use to start your own blog today" />
+<meta property="og:image"         content="https://mllj2j8xvfl0.i.optimole.com/Lsv2lkg.h4VG~36e0a/w:60/h:60/q:98/https://s15165.pcdn.co/wp-content/uploads/2018/09/adelina-1.jpg" />
+</head>
+<body>
+
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your share button code -->
+<div class="fb-share-button" 
+data-href="http://testingakshay/" 
+data-layout="button_count">
+</div>
+
+</body>
+</html>

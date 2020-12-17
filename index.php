e61a24f2644bc547b5417c7e1656fd0db77923c7
pathakakshay <pathakakshay1111@gmail.com>
2020-12-17 11:06:25 +0530

    fb

--- index.php
+++ index.php
@@ -1,32 +1,22 @@
-<html>
-<head>
-<title>Your Website Title</title>
-<!-- You can use Open Graph tags to customize link previews.
-Learn more: https://developers.facebook.com/docs/sharing/webmasters -->
-<meta property="og:url"           content="https://testingakshay.herokuapp.com/" />
-<meta property="og:type"          content="website" />
-<meta property="og:title"         content="Best free blog sites to consider in 2020" />
-<meta property="og:description" content="Looking for free blog sites? If you don't know what platform to use for your blog, check out these free blogging sites. Learn which is best.">
-<meta property="og:image" content="https://mllj2j8xvfl0.i.optimole.com/Lsv2lkg.h4VG~36e0a/w:auto/h:auto/q:98/https://s15165.pcdn.co/wp-content/uploads/2018/03/free-blogging-sites.png"></head>
-<meta property="og:image:width" content="2180">
-<meta property="og:image:height" content="1090">
-<body>
-
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your share button code -->
-<div class="fb-share-button" 
-data-href="https://testingakshay.herokuapp.com/" 
-data-layout="button_count">
-</div>
-
-</body>
-</html>
+<?php
+require_once(__DIR__.'\vendor\facebook\graph-sdk\src\Facebook\autoload.php');
+$fb = new Facebook\Facebook([
+ 'app_id' => '200198691771297',
+ 'app_secret' => '1ced23b218e39c0f6d5ddc87ab2a6f11',
+ 'default_graph_version' => 'v9.0',
+]);
+$linkData = [
+ 'link' => 'https://testingakshay.herokuapp.com/',
+ 'message' => 'Test'
+];
+$pageAccessToken = 'EAAC2FGPoU6EBAEAWJAvkS3ArVJNciZAJttdZBdryH0fQbZBSaZCjg4Q9vwAbZAhL38ffnAWpsIGCq7Op4yjrZCsJ2EgXOMZCYcJGImNnMryP2mJmZBTw43Q7UYbp9hexYaeHGEIubm5zPjtBjBdX2aJUAPdg1nMyz8EqRZBDk8T2s4JIQA7V5bil7mZCPeHeiL5KVAJnQAuk7TnZB9oTmDfGYK8';
+try {
+ $response = $fb->post('/me/feed', $linkData, $pageAccessToken);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+ echo 'Graph returned an error: '.$e->getMessage();
+ exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+ echo 'Facebook SDK returned an error: '.$e->getMessage();
+ exit;
+}
+$graphNode = $response->getGraphNode();
